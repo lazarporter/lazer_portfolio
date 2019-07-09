22396c2c7d78dd851de03d3ed1bcb2d8babd17e8
Lazer Porter <lazar.porter@gmail.com>
2019-07-08 22:28:00 -0400

    app.js changes

--- app.js
+++ app.js
@@ -1,39 +1,39 @@
-const express = require('express')
-var favicon = require('serve-favicon')
+const express = require('express');
+var favicon = require('serve-favicon');
 const bodyParser = require('body-parser');
-var morgan  = require('morgan')
-const path = require("path")
+var morgan  = require('morgan');
+const path = require("path");
 const sequelize = require('./util/database');
 
-const routes = require('./routes/index')
-// require('./controllers/contactController').initializeContacts()
+const routes = require('./routes/index');
 
-let app = express()
-var port = process.env.PORT || 8080
+let app = express();
+var port = process.env.PORT || 8080;
+
 app.use(bodyParser.urlencoded({
     extended: false
 }));
-app.use(favicon(path.join(__dirname, 'favicon.ico')))
-app.use(morgan('dev'))
-app.set('view engine', 'ejs') //templating engine
-app.set('views', 'views') //where are the templates?
+app.use(favicon(path.join(__dirname, 'favicon.ico')));
+app.use(morgan('dev'));
+app.set('view engine', 'ejs'); //templating engine
+app.set('views', 'views'); //where are the templates?
 
 //establish routes
 app.get('/', (req, res, next) => {
     res.render('home')
-})
+});
 app.get('/weather', (req, res, next) => {
     res.render('weather')
-})
-app.use('/wikipedia_viewer', routes.wikiRoutes)
-app.get('/pdftobootstrap', routes.pdftobootstrap)
-app.use('/sql-contact', routes.contactRoutes)
+});
+app.use('/wikipedia_viewer', routes.wikiRoutes);
+app.get('/pdftobootstrap', routes.pdftobootstrap);
+app.use('/sql-contact', routes.contactRoutes);
 app.use(express.static(__dirname + '/'));
 
 sequelize.sync().then(result =>{
-    app.listen(port, function () {
+    app.listen(port,  () => {
         console.log("Running contact app on port " + port)
-    })
+    });
 })
 .catch(err =>{
     console.log("Error syncing SQL: " + err);
