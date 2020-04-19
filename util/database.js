967470fa1cbd633a248b77cdcda7f73d29ba614d
Lazer Porter <lazar.porter@gmail.com>
2020-04-18 23:32:34 -0400

    adds database port to connection info

--- util/database.js
+++ util/database.js
@@ -1,6 +1,14 @@
 //create an instance for DB operations
 const Sequelize = require('sequelize');
 
-const sequelize = new Sequelize(process.env.DB_NAME, process.env.DB_USER, process.env.DB_PASSWORD, {dialect: 'mysql', host: process.env.DB_HOST});
+const sequelize = new Sequelize(
+    process.env.DB_NAME,
+    process.env.DB_USER,
+    process.env.DB_PASSWORD,
+    {
+        dialect: 'mysql',
+        host: process.env.DB_HOST,
+        port: process.env.DB_PORT
+    });
 
 module.exports = sequelize;
