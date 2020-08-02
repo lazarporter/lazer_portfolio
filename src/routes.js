f8736529d284ce9a4bdf8c55c0fb3c49e73dfda4
Lazer Porter <lazar.porter@gmail.com>
2020-08-02 12:54:18 -0400

    add vue router, dunmmy routes and components

---
+++ src/routes.js
@@ -0,0 +1,7 @@
+import Home from './components/Home.vue';
+import Projects from './components/Projects.vue';
+
+export const routes = [
+  { path: '', component: Home },
+  { path: '/projects', component: Projects },
+];
