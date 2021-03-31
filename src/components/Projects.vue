007bbff8be34e2610a9705f9e576e4666ace24b4
Lazer Porter <lazar.porter@gmail.com>
2021-03-30 23:46:24 -0400

    add router

--- src/components/Projects.vue
+++
@@ -1,9 +0,0 @@
-<template>
-  <h1>Projects</h1>
-</template>
-
-<script>
-export default {};
-</script>
-
-<style></style>
--- src/routes.js
+++
@@ -1,7 +0,0 @@
-import Home from './components/Home.vue';
-import Projects from './components/Projects.vue';
-
-export const routes = [
-  { path: '', component: Home },
-  { path: '/projects', component: Projects },
-];
